ed32f5592249053b0290a82eabf12a720f1a59b3
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-01-08 16:49:58 +0530

    pr.c
    positive or negative

--- ss.c
+++ ss.c
@@ -1,1 +1,15 @@
-#
+#include<stdio.h>
+#include<conio.h>
+void main
+{
+int a;
+printf("Enter any num=");
+scanf("%d",&a);
+if(a>0)
+  printf("The number is positive");
+else if(a<0)
+  printf("The number is negative");
+else
+  printf("The number is zero");
+getch
+}
