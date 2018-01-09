008898c45c5524030232f31967a20f9cce088ecf
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-01-09 11:10:12 -0500

    leap.c
    leap or not

---
+++ leap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\n Enter any year=");
+scanf("%d",&a);
+if(a%4==0)
+printf("\n The year is leap ");
+else
+printf("\n The number is not leap");
+getch();
+}
