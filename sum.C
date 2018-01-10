c852d4de57a90c5f337fefd0f3e0736d0eab26db
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-01-10 16:51:57 +0530

    sum.C
    sum of value

---
+++ sum.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b=0,i=0;
+printf("\n Enter n number");
+scanf("%d",&a);
+for(i=0;i<=a;i=i+1)
+{
+b=b+i;
+}
+printf("\n sum of value=%d",&b);
+getch();
+}
