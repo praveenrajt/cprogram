5453a3662550155da7d942d5008c5fa51f48512b
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-01-17 16:29:43 +0530

    vowel.C
    vowel

--- vowel.C
+++ vowel.C
@@ -2,6 +2,14 @@
 #include<conio.h>
 void main()
 {
-int a;
-printf("\n Enter the any letter");
-scanf("%d",&a);
+char a;
+printf("\n Enter the any lowecase letter");
+scanf("%c",&a);
+  if((a=a)||(a=e)||(a=i)||(a=o)||(a=u))
+  {
+    printf("\n The letter is vowel");
+  }
+  else
+    printf("\n The letter is not vowel");
+  getch();
+}
