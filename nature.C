81704b50c59ee34dec2348195371a2f4c453406a
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-01-17 16:13:31 +0530

    nature.C
    nature

---
+++ nature.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i=0,b=0;
+printf("\n Enter the any number=);
+scanf("%d",&a);
+for(i=0;i<=a;i=i+1)
+{
+  b=b+i;
+}
+printf("\n The sum nature number=%d",&a);
+getch();
+}
