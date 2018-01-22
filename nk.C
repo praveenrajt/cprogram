e538e0920c2155e9e00c6debfe58591aeeb7ee97
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-01-22 16:07:44 +0530

    Update nk.C

--- nk.C
+++ nk.C
@@ -2,17 +2,18 @@
 #include<conio.h>
 void main()
 {
-int a,b,c=0,i=0;
-printf("\n Enter the numbers=");
-scanf("%d %d",&a,&b);
-for(i=1;i<=a;i++)
-{
-printf("%d",&i);
-}
-for(i=1;i<=b;i++)
-{
-c=c+i;
-}
-printf("\n sum of num=%d",&c);
-getch();
+  int a,b,c=0,i=0;
+  printf("\n Enter the numbers=");
+  scanf("%d %d",&a,&b);
+  for(i=1;i<=a;i++)
+  {
+    printf("%d",&i);
+    b[i]=i;
+  }
+  for(i=1;i<=b;i++)
+  {
+    c=c+b[i];
+  }
+  printf("\n sum of num=%d",&c);
+  getch();
 }
