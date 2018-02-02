78567b85b47ca09f4834509a3c113cd27c363891
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-02 16:19:55 +0530

    sorted array.C
    sorted array

---
+++ sortedarray.C
@@ -0,0 +1,30 @@
+#include<stdo.h>
+#include<conio.h>
+void main()
+{
+  int a[100],n,i,j,c;
+  printf("\n Enter the no.of element=");
+  scanf("%d",&n);
+  printf("\n Enter the no=");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n-i-1;j++)
+    {
+      if(a[j]>a[j+1])
+      {
+        c=a[j];
+        a[j]=a[j+1];
+        a[j+1]=c;
+      }
+    }
+  }
+  for(i=0;i<n;i++)
+  {
+    printf("\n %d",a[i]);
+  }
+  getch();
+}
