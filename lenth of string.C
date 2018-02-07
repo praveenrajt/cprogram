a42fa521ab6490b193a8abe4862774855ebd6f1a
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-07 16:50:37 +0530

    Update lenth of string.C

--- lenth of string.C
+++ lenth of string.C
@@ -1,10 +1,19 @@
 #include<stdio.h>
 int main()
 {
+  int c=0;
   char a[10];
 printf("\n Enter string=");
-gets(a);
-printf("\n length=%d",strlen(a));
+scanf("%c",a[10]);
+  for(i=0;a[i]<'\0;i++)
+      {
+      if((a[i]>='a'&&a[i]<='z'))
+      {
+        c++;
+      }
+      }
+  
+printf("\n length=%d",c);
   
   return 0;
 }
