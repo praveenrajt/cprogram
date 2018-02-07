75417023819e594720e574d18e54f8e912a45577
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-07 16:58:43 +0530

    Fibonacci series.C

---
+++ fibonacci.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h
+void main()
+{
+  int a,i=0,b1=0,b2=1,b3;
+  printf("\n Enter limit=");
+  scanf("%d",&a);
+  for(i=1;i<=a;i++)
+  {
+    b3=b1+b1;
+    printf("\n%d",b3);
+    b1=b2;
+    b2=b3;
+  }
+  getch();
+}
+    
