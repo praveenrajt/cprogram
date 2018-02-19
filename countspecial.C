4dbda4f8a23f6d567d885b22ad2c1cf0a44d58f6
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-19 11:14:49 +0000

    countspecial.C
    count special character of string

---
+++ countspecial.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+  int c=0,i=0;
+  char s[200];
+  printf("\n Enter the string=");
+  gets(s);
+  for(i=0;i<=strlen(s);i++)
+  {
+    if(s[i]!=((s[i]>='0'&&s[i]<='9')||(s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
+    {
+      c++;
+    }
+  }
+  printf("\n no of numeric in string=%d",c);
+  getch();
+}
