342d9d6a12f4ec380917a6659b0ca244cf141be7
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-19 10:44:03 +0000

    countwords
    count no.of words

---
+++ countword.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+char s[200];
+    int c = 0, i=0;
+    printf("enter the string\n");
+    scanf("%[^\n]s", s);
+    for (i = 0;s[i] != 0;i++)
+    {
+        if (s[i] == ' ')
+            c++;    
+    }
+    printf("number of words in given string are: %d\n", c+1);
+}
