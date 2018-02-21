f554cdee0818394f18e8a33f49dca1efc9956b2f
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-21 11:25:27 +0000

    given num is power of 2
    given num is power of 2

---
+++ no is power in 2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=0,c=0,a=0;
+printf("\n Enter a=");
+scanf("%d",&a);
+for(i=1;i!=0;i++)
+{
+c=2*2;
+if(c==a)
+{
+printf("\n yes");
+break;
+}
+}
+getch();
+}
