1d850a5d17f30d3be3d336ae3322f6519f759a15
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-21 11:05:56 +0000

    sum of two numbers
    sum of two numbers

---
+++ 49.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr();
+  printf("\n Enter a,b value=");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\n sum=%d",c);
+  getch();
+}
+  
