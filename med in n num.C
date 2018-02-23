f34e11feae14215dd0ff3616961bc02f2757ebd8
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-23 15:49:37 +0530

    medium no in n numbers
    medium no in n numbers

---
+++ med in n num.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a[100],i=0,n=0;
+printf("\n Enter the n value=");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+a[i]=i;
+printf("%d",i);
+}
+printf("medium value=%d",a[n/2]).;
+return 0;
+}
