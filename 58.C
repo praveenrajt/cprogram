699e52aec053d8e23f2307bb33e7d38f2edf9eb5
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-26 16:30:06 +0530

    swap 2 num using bit operator
    swap 2 num using bit operator

---
+++ 58.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a=0,b=0;
+  printf("\n Enter a,b value=");
+  scanf("%d%d",&a,&b);
+  printf("\n before swapping of number=%d %d",a,b);
+  a=a^b;
+  b=a^b;
+  a=a^b;
+  printf("\n After swapping number=%d %d",a,b)
+}
