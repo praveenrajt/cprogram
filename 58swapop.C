e1d6abe7055552dacecacb09335f613363da37ad
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-26 16:47:19 +0530

    Update 58swapop.C

--- 58swapop.C
+++ 58swapop.C
@@ -2,11 +2,13 @@
 #include<conio.h>
 void main()
 {
-int a,b;
-printf("\n Enter a,b value=");
-scanf("%d%d",&a,&b);
-a=a^b;
-b=a^b;
-a=a^b;
-printf("\n after swapping no a=%d,b=%d",a,b);
+  int a,b;
+  printf("\n Enter a,b value=");
+  scanf("%d%d",&a,&b);
+  printf("\n Before swapping of two no=%d %d",a,b);
+  a=a^b;
+  b=a^b;
+  a=a^b;
+  printf("\n after swapping no a=%d,b=%d",a,b);
+  getch();
 }
