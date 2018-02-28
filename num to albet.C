9d14ee354a6e687717253f39384db71a898f7c48
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-28 16:44:55 +0530

    convert num to albet
    convert num to albet

---
+++ num to albet.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\n Enter any no(1-10)=");
+scanf("%d",&a);
+if(a==1)
+printf("\n ONE");
+else if(a==2)
+printf("\n TWO");
+else if(a==3)
+printf("\n THREE");
+else if(a==4)
+printf("\n FOUR");
+else if(a==5
+printf("\n FIVE");
+else if(a==6
+printf("\n SIX");
+else if(a==7)
+printf("\n SEVEN");
+else if(a==8)
+printf("\n EIGHT");
+else if(a==9)
+printf("\n NINE");
+else if(a==10)
+printf("\n TEN");
+else
+printf("\n Enter crt no");
+getch();
+}
