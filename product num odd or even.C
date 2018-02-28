6739c7014074a09bc5bc1786efbcafe73f539911
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-02-28 16:13:23 +0530

    product num is odd or even
    product num is odd or even

---
+++ product num odd or even.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n Enter a,b value=");
+scanf("%d%d",&a,&b);
+c=a*b;
+if(c%2==0);
+printf("\n EVEN");
+else
+printf("\n ODD");
+getch();
+}
