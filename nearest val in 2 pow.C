b150b5821e9076bd0663f2c7101bf9aa8b270a5f
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-03-02 16:50:20 +0530

    nearest value in power in 2
    nearest value in power in 2

---
+++ nearest val in 2 pow.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int a,b,c;
+printf("\n Enter no=");
+scanf("%d",&a);
+b=sqrt(a);
+b++;
+c=pow(b,2);
+printf("\n nearest value of power in 2=%d",c);
+return 0;
+}
