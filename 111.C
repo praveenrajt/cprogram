f3edfba8d4d0911d5a56ef9a86fc99e92a6bce8c
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-03-09 03:00:35 -0800

    count the digits
    count the digits

---
+++ 111.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=0,c=0;
+printf("Enter the no=");
+scanf("%d",a);
+while(a!=0)
+{
+a=a/10;
+c++;
+}
+printf("\n The no.of digits=%d",b);
+getch();
+}
