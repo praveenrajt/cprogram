909b8967343d866ccb8c31ccc9121093fbbceb32
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-03-14 16:56:10 +0530

    minimum number
    minimum number

---
+++ 109.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i,a[100],max,no;
+  
+  printf("\n Enter the elements=");
+  for(i=0;i<10;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  max=a[0];
+  for(i=0;i<n;i++)
+  {
+    if(a[i]<max)
+    {
+      max=a[i];
+      no=a[i];
+    }
+  }
+  printf("\n The min no is=%d",no);
+  getch();
+}
