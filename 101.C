e5f82f10efeb1097e820beef83ff575813f00dc1
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-03-14 16:04:48 +0530

    print digit in given num
    print digit in given num

---
+++ 101.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("\n Enter number a, b=");
+scanf("%d%d",&a,&b);
+while(a!=0)
+{
+d=a%10;
+c[i]=d;
+i++;
+d=a/10;
+con++:
+}
+for(j=con,i=0;i>=0,j<=con;i++,j--)
+{
+c[i]=c[j];
+}
+printf("\n no=%d",c[b]);
+getch();
+}
+
