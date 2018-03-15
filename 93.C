c2119964fa3076b4ba1267a23979fb65938d5f9d
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-03-15 21:41:05 +0530

    convert Celsius to kelvin
    convert Celsius to kelvin

---
+++ 93.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float c,k;
+printf("\n enter temperature in celsius=");
+scanf("%f",&c);
+k=c+273.15;
+printf("\n temperature in kelvin=%d",k);
+getch();
+}
