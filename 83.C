2ced0783c5ffac63f14def1c8e2968a9017a3b35
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-09-25 19:19:17 +0530

    two test case
    two test case

---
+++ 83.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,i,c,d;
+  for(i=0;i<2;i++)
+  {
+    scanf("%d / %d",&a,&b);
+    scanf("%d % %d",&c,&d);
+    printf("%d",a/b);
+    printf("%d",c%d);
+    }
+    return 0;
+    }
