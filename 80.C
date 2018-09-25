133d3b738359887eead5edc8f47b2eae5df6213f
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-09-25 19:37:00 +0530

    print odd digits
    print odd digits

---
+++ 80.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int num,rem;
+  sccanf("%d",&num);
+  while(num)
+  {
+    rem=rnum%10;
+    if(rem%2!=0)
+      printf("%d",rem);
+    num=num/10;
+   }
+   return 0;
+   }
+   
