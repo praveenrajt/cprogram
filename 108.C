87b0c7304c7eeaec1d14895b6cda0658727b35a2
praveenrajt <31438667+praveenrajt@users.noreply.github.com>
2018-09-25 19:07:40 +0530

    kth term
    kth term

--- 108.C
+++ 108.C
@@ -4,8 +4,8 @@ int main()
   int a[100],num,k;
   scanf("%d",&num);
   scanf("%d",&k);
-    for(i=1;i<=num;i+++
-    a[i]=i;
+    for(i=1;i<=num;i++)
+      scanf("%d",&a[i]);
    printf("%d",a[k]);
    return 0;
    }
